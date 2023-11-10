27dd6afe4ab4b98ef184bfca3e3ea3087ad2b91c
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-11-10 17:51:54 +0000

    Tweaked vim options and globals for gruvbox theme

--- init.lua
+++ init.lua
@@ -17,8 +17,8 @@ return {
     },
   },
   -- Set colorscheme to use
-  -- colorscheme = "gruvbox-material",
-  colorscheme = "catppuccin",
+  colorscheme = "gruvbox-material",
+  -- colorscheme = "catppuccin",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
